46dce5e6d54f32fef154236acd1105a67f86191b
Janna Hawkins <janna.bananamama@gmail.com>
2019-02-01 13:07:45 -0500

    added part two to call a method other than println on partially full array

--- partiallyFullArray.java
+++ partiallyFullArray.java
@@ -5,6 +5,7 @@ class partiallyFullArray{
 		
 		//PART ONE: ADD ELEMENTS TO PARTIALLY FILL THE ARRAY
 		//QUESTION: Does it matter when an entry is "null"?
+	    //ANSWER: Not for printing out.
 		
 		String[] array = new String[20];
 		int arrayCounter = 0;
@@ -33,15 +34,33 @@ class partiallyFullArray{
 		array[19] = "do";
 		arrayCounter++;
 		
-		for (int i = 0; i< (array.length-1); i++){
+		for (int i = 0; i< array.length; i++){
 			System.out.println("Element number " + i + " is: " +  array[i]);
 		}
 		
-		
-		
+		//PART TWO: perform an operation over the array and see what the null values produce
+		//DOES IS MATTER IF THE index points to null?
+		//YES when calling on it with a method like toUpperCase
+
+		for (int i = 0; i< (array.length-1); i++){
+		    array[i] = array[i].toUpperCase();
+			System.out.println("Element number " + i + " to Upper Case is: " +  array[i]);
+		}
 	
 		
-		//PART TWO: DELETE ELEMENT AND FILL THE HOLE TO AVOID PROCESSING A NULL ELEMENT
+		//PART TWO: Fill holes so I can call toUpperCase on a part of the array. maintain order so we end up with do-re-me-fa-so-la-ti-do and no NullPointerException
+		int counter = 0;
+		boolean isFixed = false;
+		do{
+		try {
+		    for (int i = 0; i < array.length; i++){
+			if (array[i] = null){
+			    
+			    //make code that tell you where all the null values are
+			    //then make more code that tells you how many non-null values you have
+			    //
+		    }else counter++;
+		    }}} while (isFixed = false);
 		
 		
 	}
